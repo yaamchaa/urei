57fdc4f7f0597f2b031b7096801c11dba47112bc
yaamchaa <yaamchaa@gmail.com>
2026-06-03 23:37:44 +0900

    Update Root.tsx

--- src/app/components/Root.tsx
+++ src/app/components/Root.tsx
@@ -514,7 +514,7 @@ export function Root() {
         <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-8">
           <div className="grid grid-cols-1 md:grid-cols-3 gap-8">
             <div>
-              <div className="flex items-center gap-2 mb-3">
+              <div className="w-10 h-10 flex items-center justify-center">
   <img
                   src={seongnamLogo}
                   alt="성남시 로고"
